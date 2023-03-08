5bdf14ac33e716201d57fff1da10fd1b060d1b99
Davinasha <davinaayyey>
2023-03-08 07:59:22 +0700

    komit pertama

---
+++ layout/footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <div class="container pt-5">
+    <div class="mt-5">
+    <nav class="navbar navbar-expland-Ig navbar-dark bg-dark fixed-bottom mt-5">
+        <div class="container">
+            <p class="text-light pt-3">Copyright © <?php echo date('Y');?> rendi nurdiansyah </p>
+</nav>
+</div>
+</footer>
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
